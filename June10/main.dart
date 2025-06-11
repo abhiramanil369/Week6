7f23bf4b048a9c4dc1fd782cc1575fbe012061de
abhhiramanil369 <abhiramforcoding369@gmail.com>
2025-06-11 14:47:05 +0530

    june11 file creation

---
+++ June10/main.dart
@@ -0,0 +1,16 @@
+void main(){
+    String name="Abhiram";
+    int age=20;
+    double height=5.9;
+
+    final String country="India";
+    const double pi=3.14159;
+
+
+    print("Name:$name");
+    print("Age:$age");
+    print("Height:$height");
+
+    print("Country:$country");
+    print("Pi:$pi");
+}
